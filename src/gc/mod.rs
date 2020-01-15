d4812f844214149713123ea90de8de280b487cb2
John Duimovich <jduimovich@gmail.com>
2020-01-15 15:56:19 -0500

    add iterator to memory

--- src/gc/mod.rs
+++ src/gc/mod.rs
@@ -1,16 +1,62 @@
-const MAX_MEMORY_SLOTS: usize = 1024 * 4;
-const OBJECT_HEADER_SLOTS: usize = 1;
+use std::time::SystemTime;
+
+const MAX_MEMORY_SLOTS: usize = 1024 * 128;
+pub const OBJECT_HEADER_SLOTS: usize = 1;
 
 pub struct Memory {
   head: usize,
   mem: [usize; MAX_MEMORY_SLOTS],
   roots: Vec<usize>,
   gc_count: usize,
+  allocates: usize,
+  last_gc_ms: u128,
+  total_gc_ms: u128,
   lastgc_live_mem: usize,
   lastgc_free_mem: usize,
   sum_mem_collected: usize,
-  debug: bool,
-  showHeapMap: bool,
+  show_gc: bool,
+  show_allocates: bool,
+  show_heap_map: bool,
+  show_free_list: bool,
+}
+
+impl<'a> IntoIterator for &'a Memory {
+  type Item = usize;
+  type IntoIter = MemoryIntoIterator<'a>;
+  fn into_iter(self) -> Self::IntoIter {
+    MemoryIntoIterator {
+      mem: self,
+      scan: 0,
+      free: 0,
+    }
+  }
+}
+
+pub struct MemoryIntoIterator<'a> {
+  mem: &'a Memory,
+  scan: usize,
+  free: usize,
+}
+
+impl<'a> Iterator for MemoryIntoIterator<'a> {
+  type Item = usize;
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.scan == 0 {
+      self.scan = 1;
+      self.free = self.mem.head;
+    } else {
+      self.scan = self.mem.next_object_in_heap(self.scan);
+    }
+    while self.scan == self.free {
+      self.scan = self.mem.next_object_in_heap(self.free);
+      self.free = self.mem.get_fl_next(self.free);
+    }
+    if self.scan >= MAX_MEMORY_SLOTS - 1 {
+      return None;
+    } else {
+      return Some(self.scan);
+    }
+  }
 }
 
 impl Memory {
@@ -20,11 +66,16 @@ impl Memory {
       mem: [0; MAX_MEMORY_SLOTS],
       roots: Vec::new(),
       gc_count: 0,
+      allocates: 0,
       lastgc_live_mem: 0,
       lastgc_free_mem: 0,
+      last_gc_ms: 0,
+      total_gc_ms: 0,
       sum_mem_collected: 0,
-      debug: false,
-      showHeapMap: false,
+      show_gc: false,
+      show_allocates: false,
+      show_heap_map: false,
+      show_free_list: false,
     };
     mem.set_size(0, MAX_MEMORY_SLOTS); // magic memory at zero is heap_size
     mem.set_size(mem.head, MAX_MEMORY_SLOTS - 2); // set initial object size as all heap
@@ -32,12 +83,15 @@ impl Memory {
     (mem)
   }
 
-  // objects are allocated
-  // element_size - number of slots, indexed - 0..element_size
-  // at_put - story into object slot
-  // at -- fetch slot
+  // objects API
+  // allocate_object (size) --- size is number of indexable slots
+  // add/remote_root () --- add to or remove from gc root set.
+  // element_size() - number of indexable slots - get_size() - OBJECT_HEADER_SLOTS
+  // at_put - store into object slot at index
+  // at -- fetch object slot at index
 
   pub fn allocate_object(&mut self, unrounded_size: usize) -> usize {
+    self.allocates += 1;
     let mut result = self.allocate_object_nocompress(unrounded_size);
     if result == 0 {
       self.gc();
@@ -80,19 +134,21 @@ impl Memory {
   pub fn element_size(&self, obj: usize) -> usize {
     return self.mem[obj] - OBJECT_HEADER_SLOTS;
   }
-
-  pub fn enable_debug(&mut self, enabled: bool) {
-    self.debug = enabled;
+  pub fn enable_show_heap_map(&mut self, enabled: bool) {
+    self.show_heap_map = enabled;
   }
-
-  pub fn enable_print_heap(&mut self, enabled: bool) {
-    self.showHeapMap = enabled;
+  pub fn enable_show_freelist(&mut self, enabled: bool) {
+    self.show_free_list = enabled;
   }
-
+  pub fn enable_show_gc(&mut self, enabled: bool) {
+    self.show_gc = enabled;
+  }
+  pub fn enable_show_allocates(&mut self, enabled: bool) {
+    self.show_allocates = enabled;
+  }
   fn rounded_size(unrounded_size: usize) -> usize {
     (unrounded_size + 1) & !(1) // rounded to 2
   }
-
   fn get_size(&self, obj: usize) -> usize {
     return self.mem[obj];
   }
@@ -101,12 +157,6 @@ impl Memory {
   }
   fn next_object_in_heap(&self, obj: usize) -> usize {
     return obj + self.get_size(obj);
-  }
-  fn merge_two_objects(&mut self, first: usize, second: usize) {
-    self.set_size(first, self.get_size(first) + self.get_size(second));
-  }
-  fn get_free(&self) -> usize {
-    return self.head;
   }
   //free list is linked off the first slot
   fn get_fl_next(&self, obj: usize) -> usize {
@@ -127,7 +177,7 @@ impl Memory {
 
   fn allocate_object_nocompress(&mut self, unrounded_size: usize) -> usize {
     let size = Memory::rounded_size(unrounded_size + OBJECT_HEADER_SLOTS);
-    let mut free = self.get_free();
+    let mut free = self.head;
     while free != 0 {
       let avail = self.get_size(free);
       if avail > size {
@@ -143,11 +193,22 @@ impl Memory {
         for index in 0..self.element_size(new_object) {
           self.at_put(new_object, index, 0);
         }
-        if self.debug {
+        if self.show_allocates {
           println!(
             "Success: allocate_object returning -> {} size {}",
             new_object, size
           );
+        }
+        if self.head != free {
+          if self.show_allocates {
+            println!("Reset head past intermediate free blocks \n");
+            let mut show = self.head;
+            while show != free {
+              println!("Abandon {} size {}\n", show, self.get_size(show));
+              show = self.get_fl_next(show);
+            }
+          }
+          self.head = free;
         }
         return new_object;
       }
@@ -157,17 +218,24 @@ impl Memory {
   }
 
   pub fn gc(&mut self) {
+    let start = SystemTime::now();
     for i in 0..self.roots.len() {
       self.mark_and_scan(self.roots[i]);
     }
     self.sweep();
     self.gc_count += 1;
-    
-      println!(
-        "Count {} Live  {} FreeMem {} Collected {} \n\n",
-        self.gc_count, self.lastgc_live_mem, self.lastgc_free_mem, self.sum_mem_collected
-      );
-    
+    if self.show_gc {
+      self.print_gc_stats();
+    }
+    match start.elapsed() {
+      Ok(elapsed) => {
+        self.last_gc_ms = elapsed.as_millis();
+        self.total_gc_ms += self.last_gc_ms;
+      }
+      Err(e) => {
+        println!("Error: {:?}", e);
+      }
+    }
   }
   fn sweep(&mut self) {
     let mut scan = 1;
@@ -183,13 +251,12 @@ impl Memory {
       } else {
         self.lastgc_free_mem += self.get_size(scan);
         if tail == 0 {
-          // first free object in memory order
           self.head = scan;
           self.set_fl_next(scan, 0);
           tail = scan;
         } else {
           if self.next_object_in_heap(tail) == scan {
-            self.merge_two_objects(tail, scan);
+            self.set_size(tail, self.get_size(tail) + self.get_size(scan));
           } else {
             self.set_fl_next(tail, scan);
             self.set_fl_next(scan, 0);
@@ -200,10 +267,10 @@ impl Memory {
       scan = self.next_object_in_heap(scan);
     }
     self.sum_mem_collected += self.lastgc_free_mem;
-    if self.debug {
+    if self.show_free_list {
       self.print_freelist();
     }
-    if self.showHeapMap {
+    if self.show_heap_map {
       self.print_heap();
     }
   }
@@ -219,9 +286,22 @@ impl Memory {
     }
   }
 
+  pub fn print_gc_stats(&self) {
+    println!(
+    "{} gcs, {} object allocates, Last GC: Live {} Dead {} in {} ms, Lifetime Collected {} in {} ms\n",
+    self.gc_count,
+    self.allocates,
+    self.lastgc_live_mem,
+    self.lastgc_free_mem,
+    self.last_gc_ms,
+    self.sum_mem_collected,
+    self.total_gc_ms,
+  );
+  }
+
   fn print_heap(&mut self) {
     print!("\x1B[{};{}H", 1, 1);
-    let mut free = self.get_free();
+    let mut free = self.head;
     while free != 0 {
       self.mark_object(free);
       free = self.get_fl_next(free);
@@ -245,11 +325,7 @@ impl Memory {
       }
       scan = self.next_object_in_heap(scan);
     }
-    println!("\n");
-    println!(
-      "print_heap: Count {} Live  {} FreeMem {}  Collected {} \n\n",
-      self.gc_count, self.lastgc_live_mem, self.lastgc_free_mem, self.sum_mem_collected
-    );
+    self.print_gc_stats();
   }
 
   pub fn print_freelist(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,126 @@
 mod gc;
 mod rnd;
+use std::io::stdout;
+use std::io::Write;
 
 use std::env;
 
-const LOOP: usize = 50000;
-const MAX_OBJECT_SIZE: usize = 32;
+const LOOP: usize = 20000;
+const MAX_OBJECT_SIZE: usize = 128;
 const NUMBER_OF_ROOTS: usize = 32;
 
 fn main() {
-  let args: Vec<String> = env::args().collect(); 
+
+  let args: Vec<String> = env::args().collect();
 
   let mut mem = gc::Memory::initialze_memory();
-
-  for i in 0..args.len() { 
-    if args[i] == "debug" { mem.enable_debug(true);}
-    if args[i] == "heap" { mem.enable_print_heap(true);}
+  println!( "New Heap  {} objects", mem.into_iter().count()); 
+  for obj in &mem  { 
+    println!( "New Heap, iterate over objects: {}", obj); 
   }
-  let mut objects = Vec::new();
 
-  // poof up some roots
-  for i in 0..NUMBER_OF_ROOTS {
+  for i in 0..args.len() {
+    if args[i] == "allocates" {
+      mem.enable_show_allocates(true);
+    }
+    if args[i] == "freelist" {
+      mem.enable_show_freelist(true);
+    }
+    if args[i] == "heap" {
+      mem.enable_show_heap_map(true);
+    }
+    if args[i] == "gc" {
+      mem.enable_show_gc(true);
+    }
+    if args[i] == "help" {
+      println!(
+        "GC Demo Options: 
+        \t allocates (default off) - show all allocates\n
+        \t freelist (default off) - show freelist every gc \n
+        \t heap (default off) - show heap every gc\n
+        \t gc (default off) - summary  every gc\n
+        "
+      );
+      return;
+    }
+  }
+  let root = mem.allocate_object(NUMBER_OF_ROOTS);
+  mem.add_root(root);
+  // fill in more objects off this single root
+  for i in 0..mem.element_size(root) { 
     let allocated = mem.allocate_object(rnd::rnd_sz(MAX_OBJECT_SIZE));
-    objects.push(allocated);
-    mem.add_root(allocated);
+    mem.at_put(root, i, allocated);  
   }
 
   let mut count = 0;
   while count < LOOP {
-    for i in 0..objects.len() {
-      let myobj = objects[i];
-      if i == rnd::rnd_sz(objects.len()) {
-        let allocated = mem.allocate_object(rnd::rnd_sz(MAX_OBJECT_SIZE));
-        mem.remove_root(objects[i]);
-        mem.add_root(allocated);
-        objects[i] = allocated;
+    for i in 0..mem.element_size(root) {
+      let myobj = mem.at(root,i);
+      if i == rnd::rnd_sz(mem.element_size(root)) {
+        let allocated = mem.allocate_object(rnd::rnd_sz(MAX_OBJECT_SIZE));  
+        mem.at_put(root, i, allocated);  
       } else {
         for j in 0..mem.element_size(myobj) {
           let prev = mem.at(myobj, j);
           if prev != 0 {
-            // println!(
-            //   "MyObj {} Prev {} size {}  at:0 {} at:1 {}\n", myobj, prev, mem.element_size(prev) ,
-            //   mem.at(prev, 0), mem.at(prev, 1));
-            if mem.at(prev, 0) !=  prev { panic!("object should point to self");}
-            if mem.at(prev, 1) !=  myobj { panic!("object should point to outerobj");}
+            if mem.at(prev, 0) != prev {
+              panic!("object should point to self");
+            }
+            if mem.at(prev, 1) != myobj {
+              panic!("object should point to outerobj");
+            }
+            if mem.element_size(mem.at(prev, 2)) < 2 {
+              print!("size is {}\n", mem.element_size(mem.at(prev, 2)));
+              panic!("object should be at leat 2 elements");
+            }
+            if mem.element_size(mem.at(prev, 3)) < 3 {
+              print!("size is {}\n", mem.element_size(mem.at(prev, 2)));
+              panic!("object should be at leat 2 elements");
+            }
           }
-          let slot = mem.allocate_object(2); // fill with small objects
+          let slot = mem.allocate_object(4); // fill with small objects
           mem.at_put(myobj, j, slot);
           mem.at_put(slot, 0, slot);
           mem.at_put(slot, 1, myobj);
+          let fill = mem.allocate_object(2);
+          mem.at_put(slot, 2, fill);
+          let fill = mem.allocate_object(3);
+          mem.at_put(slot, 3, fill);
         }
       }
     }
     count += 1;
-  }
-  for i in 0..NUMBER_OF_ROOTS { 
-    mem.remove_root(objects[i]);
-    mem.gc ();
-  }
-  mem.print_freelist();
+    if count % 100 == 0 {
+      print!("{}.", count);
+      let r = stdout().flush();
+      if r.is_err() {
+        print!("Error {} occured\n", r.unwrap_err());
+      }
+    }
+    if count % 1000 == 0 {
+      print!("\n"); 
+      mem.print_gc_stats();
+      let mut size = 0;
+      for obj in &mem  { 
+        size += mem.element_size(obj) + gc::OBJECT_HEADER_SLOTS; 
+      }
+      println!( "Current Heap has  {} objects with {} slots", mem.into_iter().count(), size); 
+    }
+  } 
 
+  mem.gc();   
+  let mut size = 0;
+  for obj in &mem  { 
+    size += mem.element_size(obj) + gc::OBJECT_HEADER_SLOTS; 
+  }
+  println!( "Post GC - Current Heap has  {} objects with {} slots", mem.into_iter().count(), size);  
+  
+  mem.remove_root(root); 
+  mem.gc();  
+  let mut size = 0;
+  for obj in &mem  { 
+    size += mem.element_size(obj) + gc::OBJECT_HEADER_SLOTS; 
+  } 
+  println!( "No Roots - Post GC Current Heap has  {} objects with {} slots", mem.into_iter().count(), size);  
+  mem.print_freelist();
 }
